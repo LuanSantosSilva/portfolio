28b82cf7fe0f12d4ea7824ff02b2704143331242
LuanSantosSilva <luan48924@gmail.com>
2023-12-18 11:54:35 -0300

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,132 @@
 import Image from 'next/image'
+import { promises as fs } from 'fs';
 
-export default function Home() {
+export default async function Home() {
+    const file = await fs.readFile(process.cwd() + '/data/projetos.json', 'utf8');
+    const data = JSON.parse(file);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+    <section className="background section-main">
+    <main className="alinhando">
+        <div className="banner">
+            <p className="frase-1"><span id="color-text-banner">OLÁ, EU SOU O </span>LUAN SANTOS</p>
+            <h1 className="palavra-banner">BACKEND</h1>
+            <h1 className="palavra-banner">DEVELOPER</h1>
+            <div className="icons">
+                <a target="_blank" href="https://www.linkedin.com/in/luan-dos-santos-35759b186/"><img src="/home/linkedin.svg" alt="Icone do Linkedin"/></a>
+                <a target="_blank" href="https://github.com/LuanSantosSilva"><img src="/home/github-2.svg" alt="Icone do Github"/></a>
+                <a target="_blank" href="/support/documento.pdf"><img src="/home/Profile.svg" alt="Icone Currículo"/></a>
+            </div>
         </div>
-      </div>
+    </main>
+    </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <section className="background section-about-me">
+    <div className="alinhando about-me">
+        <div className="main-about-me" id="about-me">
+            <div className="desc-about-me">
+                <h1>SOBRE MIM</h1>
+                <p>Tenho 19 anos, sou desenvolvedor web a cerca de 2 anos. Sou bastante persistente,
+                    isso me permite aprender novos conceitos em um intervalo pequeno de tempo. Profissionalmente atuo com a linguagem Python, porém também utilizo PHP e outras tecnologias web no geral. Mais abaixo você pode ver minhas experiências e Habilidades.
+                </p>
+            </div>
+            <div className="img-about-me">
+                <img src="/about-me/about-me.svg" alt=""/>
+            </div>
+        </div>
+    </div>
+    </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+    <section className="background section-projects">
+    <div className="alinhando projects" id="projects">
+      <h1>PROJETOS</h1>
+      {data.map((value: { demo: string, ler_mais: string, repositorio: string, image: string, project: string, description: string}, key: number) => {
+        const demo = value.demo == "" ? "invisible" : "";
+        const ler = value.ler_mais == "" ? "invisible" : "";
+        const repositorio = value.repositorio == "" ? "invisible" : "";
+
+        return (
+          <div className="card" key={key}>
+            <div className="card-img">
+              <img src={value.image} alt=""/>
+            </div>
+            <div className="card-desc">
+              <h1>{value.project}</h1>
+              <p className="desc">{value.description}</p>
+              <div className="card-icons">       
+               <abbr title="Repositório" className={repositorio}>
+                <a href="#" className="icon repositorio"><img src="projects/github-icon.svg" alt=""/>
+                  <p>Repositório</p>
+                </a>
+               </abbr>
+               <abbr title="Ler mais" className={ler}>
+                <a href="#" className="icon ler-mais"><img src="projects/reticencias.svg" alt=""/>
+                  <p>Ler mais</p>
+                </a>
+               </abbr>
+               <abbr title="Ver demo" className={demo}>
+                <a href={value.demo} className="icon demo"><img src="projects/link-externo.svg" alt=""/>
+                  <p>Ver demo</p>
+                </a>
+               </abbr>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  </section>
+
+  <section className="background section-skills">
+    <div className="alinhando skills" id="skills">
+        <h1>HABILIDADES</h1>
+        <p>Principais ferramentas e linguagens</p>
+        <div className="skills-icons">
+            <div className="skill">
+                <div className="skill-content">
+                    <img src="skills/html.svg" alt=""/>
+                    <p>HTML</p>
+                </div>
+            </div>
+            <div className="skill">
+                <div className="skill-content">
+                    <img src="skills/css.svg" alt=""/>
+                    <p>CSS</p>
+                </div>
+            </div>
+            <div className="skill">
+                <div className="skill-content">
+                    <img src="skills/python.svg" alt=""/>
+                    <p>Python</p>
+                </div>
+            </div>
+            <div className="skill">
+                <div className="skill-content">
+                    <img src="skills/php.svg" alt=""/>
+                    <p>PHP</p>
+                </div>
+            </div>
+            <div className="skill">
+                <div className="skill-content">
+                    <img src="skills/js.svg" alt=""/>
+                    <p>JavaScript</p>
+                </div>
+            </div>
+            <div className="skill">
+                <div className="skill-content">
+                    <img src="skills/postgresql.svg" alt=""/>
+                    <p>PostgreSQL</p>
+                </div>
+            </div>
+        </div>
+
+        <a href="#" className="icon-more-skill"><img src="projects/reticencias.svg" alt=""/>
+            <p>Ver mais</p>
         </a>
+    </div>
+</section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
